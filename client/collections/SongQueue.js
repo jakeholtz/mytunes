e4156b97a03fdde1af3863d950fd9a7d3f022151
Hack Reactor Student <student@hackreactor.com>
2017-04-10 20:15:15 -0700

    Passes SongModel and SongQueue Tests

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -4,6 +4,25 @@ var SongQueue = Backbone.Collection.extend({
   model: SongModel,
 
   initialize: function() {
-  }
+	this.on('add', function() {
+		if (this.at(1) === undefined) {
+			this.playFirst();
+		}
+	});
 
+	this.on('dequeue', function() {
+		this.remove();
+		this.playFirst();
+	});
+
+	this.on('ended', function() {
+		this.shift();
+		!this.at(0) ? null : this.playFirst();
+	})
+
+  },
+
+  playFirst: function() {
+  	this.at(0).play();
+  },
 });
--- client/views/SongQueueView.js
+++ client/views/SongQueueView.js
@@ -2,9 +2,16 @@
 var SongQueueView = Backbone.View.extend({
 
   initialize: function() {
+  	this.on('enqueue', function(e) {
+  		// var entry = new SongQueueEntryView();
+  		this.render();
+  	});
+  	this.on('add', this.render());
+  	this.on('remove', this.render());
   },
 
   render: function() {
+
     return this.$el;
   }
 
